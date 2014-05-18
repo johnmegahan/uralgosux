988e4864388b66f91c7b8e08cb912c08d66fbad4
Johnny Megahan <jmegahan@beatsmusic.com>
2014-05-17 17:29:24 -0700

    fix some shiz

--- app/js/component/dataState.js
+++ app/js/component/dataState.js
@@ -19,14 +19,16 @@ define(function (require) {
    */
 
   function dataState() {
+    var self = this;
     var storage = {};
     var fb;
     var room;
     var users;
     var djs;
+    window.storage = storage;
 
     this.defaultAttrs({
-      fireBaseUrl: ''
+      fireBaseUrl: 'https://uralgosux.firebaseio.com/'
     });
 
     this.getGenreId = function () {
@@ -40,40 +42,49 @@ define(function (require) {
     };
 
     this.sendTrackList = function () {
-      fb.
     };
 
     this.userJoined = function (user) {
-      this.trigger('dataUserJoined', { user: user });
+      self.trigger('dataUserJoined', { user: user.val() });
     };
 
-    this.userLeft = function () {
-      this.trigger('dataUserLeft', { user: user });
+    this.userLeft = function (user) {
+      self.trigger('dataUserLeft', { user: user.val() });
+    };    
+
+    this.userChanged = function (user) {
+      self.trigger('dataUserChanged', { user: user.val() });
     };
 
-    this.userList = function () {
-      this.trigger('dataUserList', { users: user.val() });
+    this.sendUser = function (evt, msg) {
+      users.child(msg.id).val()
     };
 
-    this.remoteUserVoted function () {
+    this.sendUserList = function () {
+      self.trigger('dataUsers', { users: storage.users });
+    };
 
+    this.storeUsers = function (snapshot) {
+      storage.users = snapshot.val();
     };
 
     this.after('initialize', function () {
       this.sendGenreId();
 
       // Create a firebase connection for this instance
-      room = new new Firebase(this.attr.fireBaseUrl + storage.genreId);
+      room = new Firebase(this.attr.fireBaseUrl + storage.genreId);
+
       users = room.child('users');
       djs = room.child('djs');
 
       users.on('child_added',   this.userJoined);
       users.on('child_removed', this.userLeft);
-      users.on('value',         this.remoteUserVoted);
+      users.on('child_changed', this.userChanged);
 
-      this.on('uiNeedsGenreId', this.sendGenreId);
+      this.on('uiNeedsGenreId',   this.sendGenreId);
       this.on('uiNeedsTrackList', this.sendTrackList);
-      this.on('uiNeedsUserList', this.userList);
+      this.on('uiNeedsUserList',  this.sendUserList);
+      this.on('uiNeedsUser',      this.sendUser);
     });
   }
 
